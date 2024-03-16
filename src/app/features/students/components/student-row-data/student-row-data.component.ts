feafb926954b7fa74d61e18a2e70817e838a9e1c
Feruza Makhmudova <latipova.feruza12@gmail.com>
2024-03-16 15:10:05 +0500

    fix: clean code

---
+++ src/app/features/students/components/student-row-data/student-row-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-row-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-row-data.component.html',
+  styleUrl: './student-row-data.component.sass'
+})
+export class StudentRowDataComponent {
+
+}
